f217e8c443d79d4e13af2d87abb9c3f03daf1d49
dvj1232005-boop <dvj1232005@gmail.com>
2025-12-30 22:34:58 +0700

    Create map.js

---
+++ data/map.js
@@ -0,0 +1,12 @@
+const map={
+1:{title:"Bài 1",page:9,audio:"audio/bai1.mp3"},
+2:{title:"Bài 2",page:13},
+3:{title:"Bài 3",page:25},
+4:{title:"Bài 4",page:35},
+5:{title:"Bài 5",page:47},
+6:{title:"Bài 6",page:59},
+7:{title:"Bài 7",page:71},
+8:{title:"Bài 8",page:83},
+9:{title:"Bài 9",page:93},
+10:{title:"Bài 10",page:103}
+};
